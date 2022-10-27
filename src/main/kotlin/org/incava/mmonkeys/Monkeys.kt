bbf30eff6705dff3216bc4ac37a246ab90b7359e
Jeff Pace <jeugenepace@gmail.com>
2022-10-27 07:15:37 -0400

    Merge Monkey classes

--- src/main/kotlin/org/incava/mmonkeys/Monkeys.kt
+++ src/main/kotlin/org/incava/mmonkeys/Monkeys.kt
@@ -1,19 +1,90 @@
 package org.incava.mmonkeys
 
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
+import org.incava.mmonkeys.match.MatchData
 import org.incava.mmonkeys.match.Matcher
+import org.incava.mmonkeys.util.Memory
+import java.util.concurrent.atomic.AtomicBoolean
+import java.util.concurrent.atomic.AtomicLong
 
 class Monkeys<T>(
     private val list: List<Monkey>,
     private val sought: T,
     private val matching: (Monkey, T) -> Matcher,
-    showMemory: Boolean,
-) : BaseMonkeys(showMemory) {
-    override fun CoroutineScope.launchMonkeys() = list.map { monkey ->
+    private val showMemory: Boolean,
+) {
+    private val iterations = AtomicLong(0L)
+    private val found = AtomicBoolean(false)
+    private val monitorInterval = 10_000L
+    private val maxAttempts = 100_000_000L
+
+    fun run(): Long {
+        val memory = Memory()
+        runBlocking {
+            val timer = launchTimer(memory)
+            val jobs = launchMonkeys()
+            val watcher = launchWatcher(jobs)
+            jobs.forEach { it.join() }
+            timer.cancel()
+            watcher.cancel()
+            if (showMemory) {
+                memory.showCurrent(iterations)
+            }
+        }
+//        Console.info("found?", found.get())
+//        Console.info("iterations", iterations.get())
+        return if (found.get()) iterations.get() else -1
+    }
+
+    private fun CoroutineScope.launchMonkeys() = list.map { monkey ->
         launch {
             val matcher = matching(monkey, sought)
             runMatcher(matcher)
         }
+    }
+
+    private fun CoroutineScope.launchTimer(memory: Memory): Job {
+        return launch {
+            if (showMemory) {
+                memory.monitor(iterations, monitorInterval)
+            }
+        }
+    }
+
+    private fun CoroutineScope.launchWatcher(jobs: List<Job>): Job {
+        return launch {
+            while (!found.get()) {
+                delay(1000L)
+            }
+            jobs.forEach {
+                it.cancel()
+            }
+        }
+    }
+
+    private suspend fun runMatcher(matcher: Matcher) {
+        (0 until maxAttempts).forEach { attempt ->
+            if (found.get() || checkMatcher(matcher, attempt)) {
+                return
+            }
+        }
+    }
+
+    fun onMatch(monkey: Monkey, match: MatchData, attempt: Long, iterations: Long) {
+    }
+
+    private suspend fun checkMatcher(matcher: Matcher, attempt: Long): Boolean {
+        iterations.incrementAndGet()
+        val md = matcher.check()
+        if (md.isMatch) {
+//            Console.info("success", matcher.monkey.id)
+//            Console.info("attempt", attempt)
+//            Console.info("iterations", iterations.get())
+            found.set(true)
+            return true
+        } else {
+            delay(5L)
+        }
+        return false
     }
 }
