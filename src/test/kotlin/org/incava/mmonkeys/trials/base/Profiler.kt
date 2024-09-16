10511c4a53bf674be48bf5d8b6d645e351c3b825
Jeff Pace <jeugenepace@gmail.com>
2024-09-16 08:13:53 -0400

    Show index of profile option

--- src/test/kotlin/org/incava/mmonkeys/trials/base/Profiler.kt
+++ src/test/kotlin/org/incava/mmonkeys/trials/base/Profiler.kt
@@ -3,7 +3,6 @@ package org.incava.mmonkeys.trials.base
 import org.incava.ikdk.io.Console
 import org.incava.time.Durations
 import java.time.Duration
-import kotlin.reflect.KFunction
 
 typealias DurationFunction = () -> Duration
 
@@ -24,9 +23,13 @@ open class Profiler(val numInvokes: Long, val trialInvokes: Int) {
     }.second
 
     fun runAll(): Map<String, List<Duration>> {
+        val indexed = functions.entries.withIndex().associate { it.index to it.value }
+        indexed.forEach { (index, entry) ->
+            println("$index - ${entry.key}")
+        }
         repeat(trialInvokes) { trialInvoke ->
             print("$trialInvoke / $trialInvokes")
-            functions.entries.shuffled().onEachIndexed { index, entry ->
+            indexed.entries.shuffled().forEach { (index, entry) ->
                 print(" . $index")
                 val duration = entry.value()
                 durations.computeIfAbsent(entry.key) { mutableListOf() }.also { it.add(duration) }
