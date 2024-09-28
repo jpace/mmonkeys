19837f150578d28c3ec5c39fa7fd8c13bea4fcd8
Jeff Pace <jeugenepace@gmail.com>
2024-09-28 12:30:09 -0400

    Add string factory

---
+++ src/test/kotlin/org/incava/mmonkeys/trials/rand/StringFactory.kt
@@ -0,0 +1,30 @@
+package org.incava.mmonkeys.trials.rand
+
+object StringFactory {
+    fun buildString(length: Int, charProvider: () -> Int): String {
+        val sb = StringBuilder()
+        repeat(length) {
+            val ch = 'a' + charProvider()
+            sb.append(ch)
+        }
+        return sb.toString()
+    }
+
+    fun bufferString(length: Int, charProvider: () -> Int): String {
+        val sb = StringBuffer(length)
+        repeat(length) {
+            val n = charProvider()
+            sb.append('a' + n)
+        }
+        return sb.toString()
+    }
+
+    fun assembleString(length: Int, charProvider: () -> Int): String {
+        val bytes = ByteArray(length)
+        repeat(length) { index ->
+            val n = charProvider()
+            bytes[index] = ('a' + n).toByte()
+        }
+        return String(bytes)
+    }
+}
